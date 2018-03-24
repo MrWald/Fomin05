7bf7233bb7b477c3224c07071c1d71770f6e26ec
Volodymyr <w260698@gmail.com>
2018-03-24 18:08:50 +0200

    Values are now updated every time

--- Fomin05/Process.cs
+++ Fomin05/Process.cs
@@ -3,55 +3,93 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Management;
-using System.Threading;
-using System.Windows.Documents;
 
 namespace Fomin05
 {
     class Process
     {
-        public string Name { get; }
-        public int Id { get; }
-        public bool IsActive { get; }
-        public float CpuTaken { get; }
-        public string RamTaken { get; }
-        public int ThreadsNumber { get; }
-        public string Username { get; }
-        public string FilePath { get; }
-        public string RunOn { get; }
+        private readonly PerformanceCounter _ramCounter;
+        private readonly PerformanceCounter _cpuCounter;
+        private readonly System.Diagnostics.Process _process;
 
-        internal Process(System.Diagnostics.Process systemProcess)
+        #region Properties
+        public string Name
         {
-            Name = systemProcess.ProcessName;
-            Id = systemProcess.Id;
-            IsActive = systemProcess.Responding;
-            PerformanceCounter ramCounter = new PerformanceCounter("Process", "Working Set", systemProcess.ProcessName);
-            PerformanceCounter cpuCounter = new PerformanceCounter("Process", "% Processor Time", systemProcess.ProcessName);
-            CpuTaken = cpuCounter.NextValue();
-            //Thread.Sleep(500);
-            CpuTaken = cpuCounter.NextValue();
-            RamTaken = $"{ramCounter.NextValue() / 1024 / 1024} MB";
-            ThreadsNumber = systemProcess.Threads.Count;
-            Username = GetProcessOwner(systemProcess.Id);
-            try
+            get { return _process.ProcessName; }
+        }
+
+        public int Id
+        {
+            get { return _process.Id; }
+        }
+
+        public bool IsActive
+        {
+            get { return _process.Responding; }
+        }
+
+        public string CpuTaken
+        {
+            get
             {
-                FilePath = systemProcess.MainModule.FileName;
-            }
-            catch (Win32Exception e)
-            {
-                FilePath = e.Message;
-            }
-            
-            try
-            {
-                RunOn = systemProcess.StartTime.ToString();
-            }
-            catch (Win32Exception accessDeniedException)
-            {
-                RunOn = accessDeniedException.Message;
+                return $"{_cpuCounter.NextValue()} %";
             }
         }
-        public string GetProcessOwner(int processId)
+
+        public string RamTaken
+        {
+            get { return $"{_ramCounter.NextValue() / 1024 / 1024} MB"; }
+        }
+
+        public int ThreadsNumber
+        {
+            get { return _process.Threads.Count; }
+        }
+
+        public string Username
+        {
+            get { return GetProcessOwner(_process.Id); }
+        }
+
+        public string FilePath
+        {
+            get
+            {
+                try
+                {
+                    return _process.MainModule.FileName;
+                }
+                catch (Win32Exception e)
+                {
+                    return e.Message;
+                }
+            }
+        }
+
+        public string RunOn
+        {
+            get
+            {
+                try
+                {
+                    return _process.StartTime.ToString();
+                }
+                catch (Win32Exception e)
+                {
+                    return e.Message;
+                }
+            }
+        }
+#endregion
+
+        internal Process(ref System.Diagnostics.Process systemProcess)
+        {
+            _process = systemProcess;
+            _ramCounter = new PerformanceCounter("Process", "Working Set", _process.ProcessName);
+            _cpuCounter = new PerformanceCounter("Process", "% Processor Time", _process.ProcessName);
+        }
+
+        private string GetProcessOwner(int processId)
         {
             string query = "Select * From Win32_Process Where ProcessID = " + processId;
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
@@ -70,13 +108,14 @@ namespace Fomin05
 
             return "NO OWNER";
         }
+
         internal static IEnumerable<Process> GetProcesses()
         {
             System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
             List<Process> myProcesses = new List<Process>(processes.Length);
-            foreach (System.Diagnostics.Process process in processes)
+            for (int i=0;i<processes.Length;++i)
             {
-                myProcesses.Add(new Process(process));
+                myProcesses.Add(new Process(ref processes[i]));
             }
             return myProcesses;
         }
